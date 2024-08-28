0c5d3f0bfb043fe87746f6492c5de7d681eceabe
Raavi Kaur <raavi211299@gmail.com>
2024-08-28 17:04:27 -0500

    add skelton code in main file

---
+++ main.py
@@ -0,0 +1,8 @@
+def game():
+    answer = input("Would you like to play the game?(y/n)")
+    if answer.lower() == 'y':
+        print("Welcome to the game")
+    else:
+        print("Okay, some other time")
+
+game()
